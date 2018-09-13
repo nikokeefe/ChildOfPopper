2c9a782c385514faf24aadf54d2e2281278e51ef
Nik O'Keefe <nikokeefe3@gmail.com>
2018-09-13 20:17:10 +1200

    moved post nav to before comment in single.php

--- wp-content/themes/popper-child/single.php
+++ wp-content/themes/popper-child/single.php
@@ -16,14 +16,7 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-			<?php
+      <?php
 			// Previous/next post navigation.
 			the_post_navigation( array(
 				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popper' ) . '</span> ' .
@@ -34,6 +27,15 @@ get_header(); ?>
 					'<span class="post-title">%title</span>',
 			) );
 			?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
+
 
 		<?php endwhile; // End of the loop. ?>
 
